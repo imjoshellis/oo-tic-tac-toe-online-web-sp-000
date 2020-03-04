0820f3349f892eb804bd22db9d99a9ade3bd8d00
Josh Ellis <joshe523@gmail.com>
2020-03-04 07:09:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -80,9 +80,9 @@ class TicTacToe
   end
 
   def winner
-    return nil if !self.won?
-    if self.over? && self.won?
-      return self.current_player == "X" ? "O" : "X"
+    return nil if !won?
+    if over? && won?
+      return current_player == "X" ? "O" : "X"
     end
   end
 end
